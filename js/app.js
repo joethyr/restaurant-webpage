8f264a3dae8e9c892ecef99f3e724714fe6d35c3
joethyr <joethayr@gmail.com>
2022-07-29 21:13:53 -0600

    add modal for login in button

--- js/app.js
+++ js/app.js
@@ -3,3 +3,10 @@ $(window).scroll(function () {
   $("nav").toggleClass("scrolled", $(this).scrollTop() > 50);
   $("nav a").toggleClass("scrolled", $(this).scrollTop() > 50);
 });
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
